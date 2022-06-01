58c6d3d64d61470bf053cb75999ed3be191cbd0c
evidenciasponao@gmail.com <evidenciasponao@gmail.com>
2022-06-01 23:48:41 +0200

    first commit

---
+++ client/src/routers/AppRouter.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navigation from '../components/Navigation';
+import AccountPage from '../pages/AccountPage';
+import UsersPage from '../pages/admin/UsersPage';
+import Homepage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage.js';
+import NotFoundPage from '../pages/NotFoundPage';
+import ProjectPage from '../pages/ProjectPage';
+import ProjectsPage from '../pages/ProjectsPage';
+import RegisterPage from '../pages/RegisterPage';
+import Layout from '../components/Layouts/Layout';
+
+export default function AppRouter() {
+  return (
+    <Router>
+        <Layout>
+        <Routes>
+            <Route exact path="/" element={<Homepage/>} />
+            <Route exact path="/login" element={<LoginPage/>} />
+            <Route exact path="/register" element={<RegisterPage/>} />
+            <Route exact path="/account" element={<AccountPage/>} />
+            <Route exact path="/projects" element={<ProjectsPage/>} />
+            <Route exact path="/project/:projectId" element={<ProjectPage/>} />
+            <Route exact path="/admin/:users" element={<UsersPage/>} />
+
+            <Route path="*" element={<NotFoundPages/>} />
+        </Routes>
+        </Layout>
+    </Router>
+  );
+}
